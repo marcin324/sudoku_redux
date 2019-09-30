3d556e66416f42580226b754ccff968a48f29668
Marcin Olszowski <olszyna38@gmail.com>
2019-09-30 21:00:53 +0200

    Updates

---
+++ precache-manifest.b73c38076c38f97c3224367466f66cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fa8e6a2d69afb0931f56358599cb15",
+    "url": "/sudoku_redux/index.html"
+  },
+  {
+    "revision": "3fc53ebd1d73463ea445",
+    "url": "/sudoku_redux/static/css/main.b78f85b9.chunk.css"
+  },
+  {
+    "revision": "9c14a46c88504a83a8e5",
+    "url": "/sudoku_redux/static/js/2.474be335.chunk.js"
+  },
+  {
+    "revision": "3fc53ebd1d73463ea445",
+    "url": "/sudoku_redux/static/js/main.1b98745d.chunk.js"
+  },
+  {
+    "revision": "52c9acdb96e8a4526683",
+    "url": "/sudoku_redux/static/js/runtime-main.0953496f.js"
+  }
+]);
